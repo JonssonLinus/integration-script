0e24bffb48e6bdb1cfe0f920938a26a3aec8f76d
Linus Jönsson <linus.jonsson@ki.se>
2023-06-22 17:21:28 +0200

    updated again...

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, request
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('patient_email', methods=['GET'])
-def process_url(patient_email):
-    # Use the script_parameter in your script execution
-    # For example, print it to the console
+@app.route('/process_url', methods=['GET'])
+def process_url():
+    # Get the URL parameter from the request
     url = request.args.get('url')
 
-    # Execute your script using the information from the URL
-    # Add your script execution logic here
+    # Perform any necessary processing with the URL
 
-    return redirect(url_for('http://www.google.com'))
+    # Redirect the user's browser to a new URL
+    return redirect(url_for('new_page'))
+
+@app.route('/new_page')
+def new_page():
+    return "This is the new page!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
