2e074d715999c30be3591456d90cc5651160f312
Linus Jönsson <linus.jonsson@ki.se>
2023-06-25 16:14:33 +0200

    add create patient

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask, redirect, url_for, request
+#Only log on to survey system
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.support.wait import WebDriverWait
+
+import pandas as pd
+import os
+import re
 
 app = Flask(__name__)
 
@@ -15,6 +25,24 @@ def process_url():
 @app.route('/new_page')
 def new_page():
     return "Thank you for completing the survey!"
+
+@app.route('/create_patient')
+def create_patient()
+    options = ChromeOptions()
+    #options.add_argument("--headless=new") #Comment out this line to deactivate headless option
+    driver=webdriver.Chrome(options)
+    login_url='https://svara.enkatfabriken.com/b/index.php?r=admin/authentication/sa/login'
+
+    driver.get(login_url)
+    user = driver.find_element(By.ID,'user')
+    user.send_keys('Narcolepsy')
+    pwd = driver.find_element(By.ID,'password')
+    pwd.send_keys('LaWNuS5v8aXA')
+
+    loginbutton=driver.find_element(By.XPATH,'/html/body/div[5]/div/div/div/div/form/div[2]/div/p/button')
+    loginbutton.click()
+    return(driver)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
